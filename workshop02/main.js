6e29550eb10a9ccc7e32886454914d41191f19b1
kschan88 <kschan3113@gmail.com>
2019-07-16 17:19:50 +0800

    Workshop02 Commit

--- workshop02/main.js
+++ workshop02/main.js
@@ -27,86 +27,83 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
-app.get('/api/states',(req, resp)=> {
-		// content-type: application/json
-		resp.type('application/json')
-		db.findAllStates()
-			.then(result => {
-				// 200 OK
-				resp.status(200)
-				resp.json(result);
-			})
-			.catch(error => {
-				//400 Bad request
-				resp.status(400)
-				resp.json({error: error})
-			});
-	}
-);
+app.get('/api/states', (req, resp) => {
+    // content-type: application/json
+    resp.type('application/json')
+    db.findAllStates()
+        .then(result => {
+            // 200 OK
+            resp.status(200)
+            resp.json(result);
+        })
+        .catch(error => {
+            //400 Bad request
+            resp.status(400)
+            resp.json({ error: error })
+        });
+});
 
 
 // TODO GET /api/state/:state
 app.get('/api/state/:state', (req, resp) => {
-		const stateAbbrev = req.params.state;
-		
-		resp.type('application/json')
-		db.findAllStates()
-			.then(result => {
-				if (result.indexOf(stateAbbrev.toUpperCase()) < 0) {
-					resp.status(404)
-					resp.json({ error: `Not a valid state: '${stateAbbrev}'`})
-					return;
-				}
-				return (db.findCititesState(stateAbbrev))
-			})
-		
-			.then(result => {
-				// 200 OK
-				resp.status(200)
-				resp.json(result.map(v => `/api/city/${v}`));
-				
-			})
-			.catch(error => {
-				//400 Bad request
-				resp.status(400)
-				resp.json({error: error})
-				
-			});
-	
-	}
-);
+    const stateAbbrev = req.params.state;
+
+    resp.type('application/json')
+    db.findAllStates()
+        .then(result => {
+            if (result.indexOf(stateAbbrev.toUpperCase()) < 0) {
+                resp.status(404)
+                resp.json({ error: `Not a valid state: '${stateAbbrev}'` })
+                return;
+            }
+            return (db.findCititesState(stateAbbrev))
+        })
+
+    .then(result => {
+            // 200 OK
+            resp.status(200)
+            resp.json(result.map(v => `/api/city/${v}`));
+
+        })
+        .catch(error => {
+            //400 Bad request
+            resp.status(400)
+            resp.json({ error: error })
+
+        });
+
+});
 
 
-// TODO GET /api/city/:cityId
+// TODO GET /api/city/:cityId 
 app.get('/api/city/:CityId', (req, resp) => {
-		const city_id = req.params.cityId;
-		
-		resp.type('application/json')
-		db.findCityById()
-			.then(result => {
-				if (result.indexOf(city_id.toUpperCase()) < 0) {
-					resp.status(404)
-					resp.json({ error: `Not a valid city: '${city_id}'`})
-					return;
-				}
-				return (db.findCityById(city_id))
-			})
-		
-			.then(result => {
-				// 200 OK
-				resp.status(200)
-				resp.json(result);
-				
-			})
-			.catch(error => {
-				//400 Bad request
-				resp.status(400)
-				resp.json({error: error})
-				
-			});
-	
-	}
-);
+    const city_id = req.params.cityId;
+
+    resp.type('application/json')
+    db.findCityById()
+        .then(result => {
+            if (result.indexOf(city_id.toUpperCase()) < 0) {
+                resp.status(404)
+                resp.json({ error: `Not a valid city: '${city_id}'` })
+                return;
+            }
+            return (db.findCityById(city_id))
+        })
+
+    .then(result => {
+            // 200 OK
+            resp.status(200)
+            resp.json(result);
+
+        })
+        .catch(error => {
+            //400 Bad request
+            resp.status(400)
+            resp.json({ error: error })
+
+        });
+
+});
 
 
 // TODO POST /api/city
